9310aad98f3455c23744bcce5dd6be52c3367567
Jayakumar Mogenahalli <jayakumar.m.h@gmail.com>
2019-01-19 22:29:42 +0000

    Feedback added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule} from '@angular/material/checkbox';
 import {FormsModule } from '@angular/forms';
-
+import { ReactiveFormsModule} from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 @NgModule({
   declarations: [
     AppComponent,
@@ -52,7 +54,10 @@ import {FormsModule } from '@angular/forms';
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatSlideToggleModule
   ],
   providers: [DishService, PromotionService],
   entryComponents: [
